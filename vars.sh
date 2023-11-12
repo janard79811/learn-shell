188cb214ec2c9d12c089d295e322bb161fba32b5
Adarsh <adhiadarsh97@gmail.com>
2023-11-12 22:36:25 +0530

    Updated commands for printing the color, variables declaration & exit status to frontend.sh, backend.sh & mysql.sh.

---
+++ vars.sh
@@ -0,0 +1,4 @@
+a=10
+name=john
+name="john A"
+#used double quotes above because to declare a variable if we need to use a to z, 0 to 9, underscore,,, apart from those if any special character is used then use double quotes. Since above we have space which is will considered a special character so we used double quotes.
