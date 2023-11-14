716a58ec46c0ee4efb67de8592a86a698fa16954
Adarsh <adhiadarsh97@gmail.com>
2023-11-14 15:46:15 +0530

    Loops concept included

---
+++ 06-Loops.sh
@@ -0,0 +1,13 @@
+# While & For loop
+
+# Choose while loop if you are using expression
+# Choose For loop if you are having an inputs
+
+# loop is going to execute the same set of command/ commands again & again i.e. in iterative way/method.
+
+a=10
+while [ "$a" -gt 0 ]; do
+  echo Hello World
+  done
+
+# The above while loop is used as its an expression & the above loop will print Hello world for infinite times as everytime calling  $a =10 only.
