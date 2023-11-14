5ddb04e1bda556781d603b5630fc89be56ffc87a
Adarsh <adhiadarsh97@gmail.com>
2023-11-14 16:02:34 +0530

    Loops concept included i.e. added for loop script also along with while loop

--- 06-loop.sh
+++ 06-loop.sh
@@ -14,4 +14,9 @@ done
 
 # The above while loop is used as its an expression & the above loop will print Hello world for 10 times until $a=1
 # Provided arithmetic expression because loop will continue infinite times as everytime $a will called as 10 only
-# With sleep 1 also you can execute that's fine (but sleep 1 is given because to get the o/p each step 1 by 1 after every 1 second since sleep value given as 1
+# With sleep 1 is not mandatory & even using that also you can execute that's fine (but sleep 1 is given because to get the o/p each step 1 by 1 after every 1 second since sleep value given as 1
+
+for fruit in mango gauva grapes ; do
+  echo fruit name - $fruit
+  sleep 1
+done
